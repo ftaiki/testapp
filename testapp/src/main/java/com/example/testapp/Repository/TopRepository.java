58b82b7f9a8c3b4d7a4d61fc051828881753a6c2
taikif <hatakeyama.taiki1@gmail.com>
2023-05-28 12:21:51 +0900

    entity作成, repoでuserテーブル取得メソッド実装

--- testapp/src/main/java/com/example/testapp/Repository/TopRepository.java
+++ testapp/src/main/java/com/example/testapp/Repository/TopRepository.java
@@ -1,15 +1,29 @@
 package com.example.testapp.Repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
+import com.example.testapp.Entity.UserEntity;
 @Repository
 public class TopRepository {
-    public static String selectOne(int userId){
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    public UserEntity selectOne(int userId){
+        String sql = "SELECT * FROM users WHERE user_id=?";
+
+        Map<String, Object> map = jdbcTemplate.queryForMap("SELECT * FROM users WHERE user_id = ?", userId);
         
+        UserEntity user = new UserEntity();
+
+        user.setUserId((int) map.get("user_id"));
 
 
-
-        return "a";
+        return user;
     }
 
 }
